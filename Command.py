197063fbecda16a25e46c262cf76179a7222a0d7
prushton2 <peterrushton52@gmail.com>
2020-04-19 14:21:15 -0400

    Adding a ping command

---
+++ Command.py
@@ -0,0 +1,16 @@
+class Command:
+    def __init__(self, commands, commandName):
+        self.commands = commands
+        self.commandName = commandName
+
+
+class Commands:
+    def __init__(self):
+        self.commands = []
+    def checkAllCommands(self, message, prefix):
+        for i in self.commands:
+            for j in i.commands:
+                if(message.content.startswith(prefix+j)):
+                    return i.commandName
+
+
--- main.py
+++ main.py
@@ -1,14 +1,38 @@
 import discord
 from discord.ext import commands
 import os
+
 json = __import__("JsonManager")
+Comm = __import__("Command")
 
-client = commands.Bot(command_prefix= "enc.")
-config = json.JsonManager(os.path.join(os.getcwd(),"Discordbot\config.json"))
+cmds = Comm.Commands()
+cmds.commands = [Comm.Command(['ping', 'test'], "ping")]
 
 
-@client.event
+bot = commands.Bot(command_prefix= "enc.")
+
+config = json.JsonManager(os.path.join(os.getcwd(),"Discordbot/config.json"))
+
+
+@bot.event
 async def on_ready():
     print("Bot is ready")
 
-client.run(config.load()["token"])
+@bot.event
+async def on_message(message):
+
+    
+    if(message.author.id == bot.user.id):
+        return
+
+    if(message.content.startswith(config.load()["prefix"])):
+        command = cmds.checkAllCommands(message, config.load()["prefix"])
+
+    
+    if(command == "ping"):
+        await message.channel.send("Pong")
+
+
+
+bot.run(config.load()["token"])
+
